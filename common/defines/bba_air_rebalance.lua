eecae32ff2d2732bbffb3aa6f5be296adf66442a
Alex <sturmmanngr@gmail.com>
2023-02-01 19:21:03 +0300

    alt planes mod version

---
+++ common/defines/bba_air_rebalance.lua
@@ -0,0 +1,3 @@
+NDefines.NCountry.AIR_SUPPLY_CONVERSION_SCALE = 0.02;					-- Conversion scale for planes to air supply
+NDefines.NAir.AIR_WING_MAX_STATS_SPEED = 1000
+NDefines.NAir.AIR_WING_COUNTRY_XP_FROM_TRAINING_FACTOR = 0.01
